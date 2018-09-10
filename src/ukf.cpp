57239b8ac6ae1cba0d7c815cf5fc95ff9637f801
fuqiang07 <36307008+fuqiang07@users.noreply.github.com>
2018-09-10 15:29:18 -0500

    Add files via upload

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -166,9 +166,9 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
 
       P_ << 0.1, 0, 0, 0, 0,
             0, 0.1, 0, 0, 0,
-            0, 0, 5, 0, 0,
-            0, 0, 0, 5, 0,
-            0, 0, 0, 0, 5;
+            0, 0, 10, 0, 0,
+            0, 0, 0, 10, 0,
+            0, 0, 0, 0, 10;
     }
 
     // initialize the previous_timestamp
